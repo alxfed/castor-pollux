1cb15dd6ea7ce3862fef39773131e136a31008b1
Alex Fedotov <alxfed@gmail.com>
2025-04-22 10:23:53 -0500

    one step for a man...

--- src/castor_pollux/rest.py
+++ src/castor_pollux/rest.py
@@ -75,7 +75,7 @@ garbage = [
 #         return None
 
 
-def continuation(contents, instruction, recorder=None, **kwargs):
+def continuation(text=None, contents=None, instruction=None, recorder=None, **kwargs):
     """A completions endpoint call.
         kwargs:
             temperature     = 0 to 1.0
@@ -85,41 +85,37 @@ def continuation(contents, instruction, recorder=None, **kwargs):
             max_tokens      = number of tokens
             stop            = ['stop']  array of up to 4 sequences
     """
-    instruction = {'parts': [{'text': 'You are an eloquent assistant'}], 'role': 'user'}
-    text = [
-        {'parts': [{'text': 'In one sentence, who was Gerhard Gentzen?'}], 'role': 'user'}
-    ]
+    instruction         = kwargs.get('system_instruction', instruction)
+    system_instruction  = {'parts': [{'text': instruction}], 'role': 'user'} if instruction else None
+    contents            = kwargs.get('contents', contents)
 
-    generation_config = {
-        'stopSequences': ['STOP'],
-        'responseMimeType': 'text/plain',
-        # 'responseSchema': {},
-        'responseModalities': ['TEXT'],
-        'candidateCount': 1,
-        'maxOutputTokens': 1024,
-        'temperature': 0.5,
-        'topP': 0.9,
-        'topK': 10,
-        'seed': 246,
-        # 'presencePenalty': number,
-        # 'frequencyPenalty': number,
-        # 'responseLogprobs': boolean,
-        # 'logprobs': integer,
-        # 'enableEnhancedCivicAnswers': boolean,
-        # 'speechConfig': {
-        #     object (SpeechConfig)
-        # },
-        'thinkingConfig': {
-            'includeThoughts': True,
-            'thinkingBudget': 24576
-        }
-    }
+    # Create a structure that the idiots want.
+    if text and not contents:
+        contents = [{'parts': [{'text': text}], 'role': 'user'}]
+    else:
+        contents.append({'parts': [{'text': text}], 'role': 'user'})
+
     json_data = {
-        'systemInstruction': instruction,
+        'systemInstruction': system_instruction,
         'contents': contents,
         'safetySettings':  garbage,
-        'generationConfig': generation_config,
-        # 'cachedContent': ''
+        'generationConfig':{
+             'stopSequences':   kwargs.get('stop_sequences', ['STOP','Title']),
+             'responseMimeType': kwargs.get('mime_type','text/plain'),
+            # 'responseSchema': {},
+             'responseModalities': kwargs.get('modalities',['TEXT']),
+             'temperature':     kwargs.get('temperature', 0.5),
+             'maxOutputTokens': kwargs.get('max_tokens', 10000),
+             'candidateCount':  kwargs.get('n', 1),  # not mandatory 1 now
+             'topP':            kwargs.get('top_p', 0.9),
+             'topK':            kwargs.get('top_k', 10),
+            'enableEnhancedCivicAnswers': False,
+            'thinkingConfig': {
+                'includeThoughts': True,
+                'thinkingBudget': 24576
+                }
+        # 'cachedContent': '',
+        },
     }
     try:
         response = requests.post(
@@ -132,18 +128,22 @@ def continuation(contents, instruction, recorder=None, **kwargs):
                 raise Exception('Results filtered')
             else:
                 answer_dump = response.json()
-                if recorder:
-                    log_message = {'query': json_data, 'response': answer_dump}
-                    recorder.log_event(log_message)
+            #     if recorder:
+            #         log_message = {'query': json_data, 'response': answer_dump}
+            #         recorder.log_event(log_message)
         else:
             print(f'Request status code: {response.status_code}')
             return None
-        if recorder:
-            rec = {'messages': json_data['contents'], 'response': answer_dump['candidates']}
-            recorder.record(rec)
+        # if recorder:
+        #     rec = {'messages': json_data['contents'], 'response': answer_dump['candidates']}
+        #     recorder.record(rec)
+
     except Exception as e:
-        print(f'Unable to generate continuations response, {e}')
+        print(f'Unable to generate continuation of the text, {e}')
         return None
+
+    return answer_dump
+
 
 def embed(input_list, **kwargs):
     """Returns the embedding of a list of text strings.
@@ -174,9 +174,8 @@ if __name__ == '__main__':
     '''
     ['gemini-2.5-flash-preview-04-17', 'gemini-2.5-pro-exp-03-25', 'gemini-1.5-flash-latest','gemini-2.0-flash-lite','gemini-2.0-flash','gemini-2.0-pro-exp-02-05','gemini-2.0-flash-thinking-exp-01-21']
     '''
-    instruction = {'parts': [{'text': 'You are an eloquent assistant'}], 'role': 'user'}
-    text = [{
-    'parts':[{'text': 'In one sentence, who was Gerhard Gentzen?'}]
-    }]
-    result = continuation(text, instruction)
+    kwa = {'model': 'gemini-2.5-flash-preview-04-17', 'n':2}
+    instruction = 'You are an eloquent assistant'
+    text = 'In one sentence, who was Gerhard Gentzen?'
+    result = continuation(text, instruction=instruction, **kwa)
     print(result)
